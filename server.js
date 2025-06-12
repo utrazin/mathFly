9bc320bdc4acc6349b867fcec694cb72091ac01d
Enzo Dutra <enzorei111x@gmail.com>
2025-06-12 17:01:18 -0300

    Revert Ajustes

--- server.js
+++ server.js
@@ -43,7 +43,6 @@ app.get('/nivel_4', (req, res) => {
     res.sendFile(path.join(__dirname, 'nivel_4.html')); // Extremo
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
 });
